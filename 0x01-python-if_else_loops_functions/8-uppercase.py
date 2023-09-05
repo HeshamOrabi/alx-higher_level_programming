5e29ea56024a5983da386400f46b7e3091833a88
Hesham <dev@heshamorabi.com>
2023-09-05 14:33:16 -0700

    To uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,5 +6,3 @@ def uppercase(str):
             print("{}".format(char))
         elif ord(char) > 96 and ord(char) < 123:
             print("{}".format(chr(ord(char) - 32)))
-        else:
-            print("{}".format(char))
