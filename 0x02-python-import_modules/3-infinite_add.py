eeb17d2424959b82ee23c3ca7cd5b7e9804d52f0
Hesham <dev@heshamorabi.com>
2023-09-07 14:39:44 -0700

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
             sum += int(arg)
         except ValueError:
             print(f"Warning: Skipping invalid argument '{arg}'")
-            
+
     print(sum)
