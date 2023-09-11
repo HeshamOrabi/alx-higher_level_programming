55da6d0eb6528ba85c22153ca5f21dbfb98bb2f8
Hesham <dev@heshamorabi.com>
2023-09-11 16:01:27 -0700

    Replace in a copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    if my_list:
-        new_list = my_list
+    new_list = my_list
 
-        new_list[idx] = element
-        return new_list
+    new_list[idx] = element
+    return new_list
