22a014f1d5231a1bed30328021320ce6e482c8f7
Hesham <dev@heshamorabi.com>
2023-09-11 17:56:49 -0700

    More returns!

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,8 +2,8 @@
 
 def multiple_returns(sentence):
     tub = ()
-    if sentence[0] == '':
-        sentence[0] = None
+    if sentence == "":
+        return (0, None)
     tub = len(sentence), sentence[0]
 
     return tub
