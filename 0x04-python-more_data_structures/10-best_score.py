e98c035400dee09a1b0b75b55a797f8f69cbfdec
Hesham <dev@heshamorabi.com>
2023-09-14 01:49:19 -0700

    Best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    if a_dictionary == None:
-        return None
-    return max(a_dictionary, key=lambda k: a_dictionary[k])
+    return max(a_dictionary, key=lambda k: a_dictionary[k], default=None)
