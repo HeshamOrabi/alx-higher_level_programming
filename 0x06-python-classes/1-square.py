9fbdf161d0e34983a160662ab01b21010e24a664
Hesham <dev@heshamorabi.com>
2023-09-26 19:55:13 +0000

    Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 1-square.py by Hesham Orabi
+"""Defines a square """
+
+
+class Square:
+    """This class represents a square shape."""
+
+	def __init__(self, size):
+		"""Initializing this class
+		Args: size - represnets the size of the square
+		"""
+		self.__size = size
