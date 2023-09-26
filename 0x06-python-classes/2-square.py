07b002a51ab30783acb298ca05c249009af7fafe
Hesham <dev@heshamorabi.com>
2023-09-26 20:42:12 +0000

    Size validation

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 # 0-square.py by Hesham Orabi
-"""Defines a square """
+"""A module that defines a square"""
 
 
 class Square:
-    """Represents a square"""
+    """Class represents a square"""
 
     def __init__(self, size):
         """Initializing this square class
