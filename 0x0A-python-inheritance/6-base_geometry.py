7dcafc1a90c4a9d118d35b859e243a98f928c89e
Hesham <dev@heshamorabi.com>
2023-10-10 09:20:25 -0700

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+
+    improved BaseGeometry
+
+"""
+
+
+class BaseGeometry:
+    """improved BaseGeometry"""
+    def area(self):
+        raise Exception('area() is not implemented')
