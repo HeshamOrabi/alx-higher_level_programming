05a827e1751632047eac2d687fe485d5f047e98b
Hesham <dev@heshamorabi.com>
2023-10-11 14:33:42 -0700

    9-student.py

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+
+    class Student
+
+"""
+
+
+class Student:
+    """A class to define student"""
+    def __init__(self, first_name, last_name, age):
+        """Intializing..."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """Return a dict representation"""
+        return self.__dict__
