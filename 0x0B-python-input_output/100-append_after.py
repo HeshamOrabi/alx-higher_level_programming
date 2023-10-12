84f4060899ed2a97fcba39c129f10ddc252144a6
Hesham <dev@heshamorabi.com>
2023-10-11 18:41:09 -0700

    Search and update

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+
+    function that inserts a line of text to a file
+
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """function that inserts a line of text to a file"""
+    txt = ""
+    with open(filename, "r", encoding="UTF-8") as f:
+        for line in f:
+            txt += line
+            if search_string in line:
+                txt += new_string
+    with open(filename, "w", encoding="UTF-8") as f:
+        f.write(txt)
+        
