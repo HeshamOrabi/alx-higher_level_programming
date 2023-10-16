cbb0b5a33e94df8c47221d6ea215572e648e12e8
Hesham <dev@heshamorabi.com>
2023-10-16 16:05:26 -0700

    Hot fix-> 4

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -29,7 +29,7 @@ class Rectangle(Base):
 
     def update(self, *args, **kwargs):
         """update values"""
-        if args:
+        if args is not None and len(args) != 0:
             if len(args) >= 1:
                 self.id = args[0]
             if len(args) >= 2:
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,7 +25,7 @@ class Square(Rectangle):
 
     def update(self, *args, **kwargs):
         """set args"""
-        if args:
+        if args is not None and len(args) != 0:
             if len(args) == 1:
                 self.id = args[0]
             if len(args) == 2:
