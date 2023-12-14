66a5e60f67c7968af089b75592a4cedbac831a12
Hesham <Dev@heshamorabi.com>
2023-12-15 01:39:39 +0200

    Square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
+  console.log('Missing size')
+} else {
+  for (let i = args[0]; i > 0; i--) {
+    let line = ''
+    for (let j = args[0]; j > 0; j--) {
+      line += 'x';
+    }
+    console.log(line);
+  }
+}
