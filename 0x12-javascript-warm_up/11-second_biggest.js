0b78d9236bf3c22f1ac55c4baf461fd37855cecb
Hesham <Dev@heshamorabi.com>
2023-12-15 03:28:47 +0200

    Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,10 +6,5 @@ if (args.length < 2) {
   console.log(0);
 } else {
   const newArgs = args.sort((a, b) => b - a);
-
-  if (newArgs.length === 2) {
-    console.log(newArgs[0]);
-  } else {
-    console.log(newArgs[1]);
-  }
+  console.log(newArgs[1]);
 }
